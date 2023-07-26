6c5ac06b0bbdd544ce9fa617e8c5237e962702bd
Iamstefanoe <stefanoar3@gmail.com>
2023-07-26 15:25:55 +0200

    chore: Profile layout fixes& mobile/desktop resp

--- src/components/myBalance/MyBalance.jsx
+++ src/components/myBalance/MyBalance.jsx
@@ -3,27 +3,27 @@ import styles from "./index.module.scss";
 import Chart from "@/components/chart";
 
 const MyBalance = () => {
-	const [balance, setBalance] = useState(null);
+  const [balance, setBalance] = useState(null);
 
-	useEffect(() => {
-		const getUserData = () => {
-			const userDataString = localStorage.getItem("users");
-			if (userDataString) {
-				const userDataObject = JSON.parse(userDataString);
-				setBalance(userDataObject.balance);
-			}
-		};
+  useEffect(() => {
+    const getUserData = () => {
+      const userDataString = localStorage.getItem("users");
+      if (userDataString) {
+        const userDataObject = JSON.parse(userDataString);
+        setBalance(userDataObject.balance);
+      }
+    };
 
-		getUserData();
-	}, []);
+    getUserData();
+  }, []);
 
-	return (
-		<div className={styles.MyBalance}>
-			<h1>My Balance</h1>
-			<p>{balance}</p>
-			<Chart />
-		</div>
-	);
+  return (
+    <div className={styles.MyBalance}>
+      <h1>My Balance</h1>
+      <p>{balance}$</p>
+      <Chart />
+    </div>
+  );
 };
 
 export default MyBalance;
