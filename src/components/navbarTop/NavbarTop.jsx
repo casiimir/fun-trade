2d5a813fbd6f4a8d52c4cf569a070fb8bcdd5dc1
mirkemme <mirkomontaina@gmail.com>
2023-07-26 18:36:14 +0200

    feature: add overlay component

--- src/components/navbarTop/NavbarTop.jsx
+++ src/components/navbarTop/NavbarTop.jsx
@@ -2,36 +2,34 @@ import { useRouter } from "next/router";
 import styles from "./NavbarTop.module.scss";
 import Menu from "../menu";
 import { useState } from "react";
+import Overlay from "../overlay/Overlay";
 
 const NavbarTop = ({ items }) => {
-	const router = useRouter();
-	const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
-	const onHandleClick = (page) => {
-		// router.push(`/${page}`);
-		setIsOpen((prev) => !prev);
-	};
+  const onHandleClick = (page) => {
+    // router.push(`/${page}`);
+    setIsOpen((prev) => !prev);
+  };
 
-	return (
-		<header className={styles.Navbar}>
-			{items?.map((item) => (
-				<div
-					key={item.id}
-					className={`${styles.container} ${
-						item?.label === "logo" ? styles.logo : ""
-					}`}
-					onClick={() => onHandleClick(item.label)}
-				>
-					{item.icon}
-				</div>
-			))}
-			{isOpen && <div className={styles.overlay} onClick={onHandleClick} />}
-
-			<div className={`${styles.burger} ${isOpen && styles.open}`}>
-				<Menu setIsOpen={setIsOpen} />
-			</div>
-		</header>
-	);
+  return (
+    <header className={styles.Navbar}>
+      {items?.map((item) => (
+        <div
+          key={item.id}
+          className={`${styles.container} ${item?.label === "logo" ? styles.logo : ""}`}
+          onClick={() => onHandleClick(item.label)}
+        >
+          {item.icon}
+        </div>
+      ))}
+      {isOpen && <Overlay setIsOpen={setIsOpen} />}
+      <div className={`${styles.burger} ${isOpen && styles.open}`}>
+        <Menu setIsOpen={setIsOpen} />
+      </div>
+    </header>
+  );
 };
 
 export default NavbarTop;
---
+++ src/components/overlay/Overlay.jsx
@@ -0,0 +1,11 @@
+import styles from "./Overlay.module.scss";
+
+const Overlay = ({ setIsOpen }) => {
+  const onHandleClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return <div className={styles.overlay} onClick={onHandleClick} />;
+};
+
+export default Overlay;
