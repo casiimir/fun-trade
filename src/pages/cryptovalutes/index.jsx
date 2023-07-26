0ac83b1134dcbcc91907cf47aa202ffab4262bd2
PeppeTll <peppetll@gmail.com>
2023-07-26 19:04:00 +0200

    feat: crypto page

---
+++ src/pages/cryptovalutes/index.jsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Card from "../../components/card";
+import styles from "./index.module.scss";
+
+import { useRouter } from "next/router";
+
+//Image
+import arrow from "../../assets/iconsProject/arrow.svg";
+
+export default function cryptovalutes() {
+	const router = useRouter();
+
+	const onHandleBack = () => {
+		router.push("/homepage");
+	};
+
+	return (
+		<div className={styles.cryptovalutes}>
+			<div className={styles.cryptovalutes__header}>
+				<Image
+					src={arrow}
+					alt="back"
+					width={30}
+					height={30}
+					onClick={onHandleBack}
+				/>
+				<h1 className={styles.cryptovalutes__header__title}>Cryptovalute</h1>
+			</div>
+			<div className={styles.cryptovalutes__list}>
+				{/* {data.map((item) => <Card data={item} />)} */}
+			</div>
+		</div>
+	);
+}
+
+// export const getServerSideProps = async () => {
+// 	const res = await fetch(
+// 		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=5&page=1&sparkline=false&locale=it"
+// 	);
+// 	const data = await res.json();
+// 	return {
+// 		props: {
+// 			data,
+// 		},
+// 	};
+// };
