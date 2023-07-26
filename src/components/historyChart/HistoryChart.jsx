b65220895883278301a3249dc5fe9d1de8321998
PeppeTll <peppetll@gmail.com>
2023-07-26 12:30:33 +0200

    feat: card component

--- src/components/historyChart/HistoryChart.jsx
+++ src/components/historyChart/HistoryChart.jsx
@@ -4,56 +4,56 @@ import moment from "moment";
 /* import Skeleton from "./Skeleton"; */ //implementazione futura skeleton loader
 
 const HistoryChart = ({ id }) => {
-  const [loading, setLoading] = useState(true);
-  const [coinChartData, setCoinChartData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [coinChartData, setCoinChartData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=31`
-        );
-        const data = await response.json();
-        const formattedData = data.prices.map((value) => ({
-          x: value[0],
-          y: value[1].toFixed(2),
-        }));
-        setCoinChartData(formattedData);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [id]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=2`
+				);
+				const data = await response.json();
+				const formattedData = data.prices.map((value) => ({
+					x: value[0],
+					y: value[1].toFixed(2),
+				}));
+				setCoinChartData(formattedData);
+				setLoading(false);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, [id]);
 
-  if (loading) {
-    return <p>Loading chart data...</p>; // skeleton here
-  }
+	if (loading) {
+		return <p>Loading chart data...</p>; // skeleton here
+	}
 
-  const options = {
-    responsive: true,
-  };
+	const options = {
+		responsive: true,
+	};
 
-  const data = {
-    labels: coinChartData.map((value) => moment(value.x).format("MMM DD")),
-    datasets: [
-      {
-        fill: true,
-        label: id,
-        data: coinChartData.map((val) => val.y),
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
-  };
+	const data = {
+		labels: coinChartData.map((value) => moment(value.x).format("MMM DD")),
+		datasets: [
+			{
+				fill: false,
+				label: id,
+				data: coinChartData.map((val) => val.y),
+				borderColor: "rgb(53, 162, 235)",
+				backgroundColor: "rgba(53, 162, 235, 0.5)",
+			},
+		],
+	};
 
-  return (
-    <div>
-      <Line options={options} data={data} />
-    </div>
-  );
+	return (
+		<div>
+			<Line options={options} data={data} />
+		</div>
+	);
 };
 
 export default HistoryChart;
