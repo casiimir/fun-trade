70bf4480726e907c14fae028044dca394bfd26c1
PeppeTll <peppetll@gmail.com>
2023-07-26 16:01:22 +0200

    feat: firebase db

---
+++ src/firebase/index.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: process.env.APIKEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGE_SENDER_ID,
+	appId: process.env.APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
