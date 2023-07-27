1351be156b9b56aa85d70c7bca0078b5dd854cd8
mirkemme <mirkomontaina@gmail.com>
2023-07-27 13:40:05 +0200

    feat: add crypto search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,33 +1,43 @@
 import Image from "next/image";
 import styles from "./Search.module.scss";
 import closeIcon from "../../assets/iconsProject/close-menu.svg";
-import { useState } from "react";
-import searchIcon from "../../assets/iconsProject/search.svg";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import cryptoList from "@/mock/cryptoCardMock";
 
-const Search = ({ setIsSearchOpen }) => {
+const Search = ({ props }) => {
+  const { isSearchOpen, setIsSearchOpen } = props;
   const [inputText, setInputText] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const router = useRouter();
 
-  const onHandleClick = () => setIsSearchOpen((prev) => !prev);
-  const onHandleChange = (e) => setInputText(e.target.value);
-  const onHandleSubmit = (e) => {
-    e.preventDefault();
+  const onHandleClickClose = () => {
+    setIsSearchOpen((prev) => !prev);
     setInputText("");
+    setSearchResults([]);
   };
+  const onHandleChange = (e) => setInputText(e.target.value);
+  const onHandleClickCrypto = (id) => {
+    // router.push(`/cryptovalutes/${id}`);
+  };
+
+  useEffect(() => {
+    setInputText("");
+    setSearchResults([]);
+  }, [isSearchOpen]);
+
+  useEffect(() => {
+    if (inputText)
+      setSearchResults(
+        cryptoList.filter((crypto) => crypto.name.toLowerCase().includes(inputText))
+      );
+  }, [inputText]);
 
   return (
     <section className={styles.Search}>
-      <form className={styles.form} onSubmit={onHandleSubmit}>
-        <button onClick={onHandleClick} className={styles.closeBtn}>
+      <div className={styles.container}>
+        <button onClick={onHandleClickClose} className={styles.closeBtn}>
           <Image src={closeIcon} alt="close" width={40} height={40} />
-        </button>
-        <button type="submit" name="submit" className={styles.submitBtn}>
-          <Image
-            src={searchIcon}
-            alt="search"
-            width={30}
-            height={30}
-            className={styles.icon}
-          />
         </button>
         <input
           type="text"
@@ -38,7 +48,23 @@ const Search = ({ setIsSearchOpen }) => {
           required
           className={styles.input}
         />
-      </form>
+      </div>
+      {searchResults.length > 0 && (
+        <div className={styles.searchResults}>
+          {searchResults?.map((result) => (
+            <div
+              className={styles.card}
+              key={result.id}
+              onClick={() => onHandleClickCrypto(result.id)}
+            >
+              <div className={styles.image}>
+                <img src={result?.image} alt={result?.name} />
+              </div>
+              <p className={styles.text}>{result?.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
