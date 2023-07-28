9592e3c381cc689bc01b9f7eff47f6a256efb4b8
mirkemme <mirkomontaina@gmail.com>
2023-07-28 10:56:02 +0200

    chore: update bottom navbar

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,21 +1,16 @@
-import React, { useContext, useEffect, useState } from "react";
-import { userContext } from "@/pages";
 import NavbarTop from "@/components/navbarTop";
 import NavbarBottom from "@/components/navbarBottom";
 import { navbarItems } from "../mock/navbarItems";
 import styles from "./DefaultLayout.module.scss";
 
 const DefaultLayout = ({ children }) => {
-	// const value = useContext(userContext);
-	const [isLogged, setIsLogged] = useState(false);
-
-	return (
-		<>
-			{true && <NavbarTop items={navbarItems[0].items} />}
-			<section className={`${true && styles.content}`}>{children}</section>
-			{true && <NavbarBottom />}
-		</>
-	);
+  return (
+    <>
+      <NavbarTop items={navbarItems[0].items} />
+      <section className={`${true && styles.content}`}>{children}</section>
+      <NavbarBottom />
+    </>
+  );
 };
 
 export default DefaultLayout;
