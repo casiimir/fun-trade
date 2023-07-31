e297ba610c697961b90dd5763a7db5983aa8cf90
Iamstefanoe <stefanoar3@gmail.com>
2023-07-31 13:25:35 +0200

    chore: history transactions via localStorage

--- src/components/historyTransactions/HistoryTransactions.jsx
+++ src/components/historyTransactions/HistoryTransactions.jsx
@@ -4,8 +4,20 @@ import { UserContext } from "@/pages/_app";
 import styles from "./index.module.scss";
 
 const HistoryTransactions = () => {
-  const { userData } = useContext(UserContext);
+  /* const { userData } = useContext(UserContext);
   console.log(userData);
+ */
+  const [hTrans, sethTrans] = useState(null);
+  useEffect(() => {
+    const getUserData = () => {
+      const userDataString = localStorage.getItem(transition);
+      if (userDataString) {
+        const userDataObject = JSON.parse(userDataString);
+        sethTrans(userDataObject.htrans);
+      }
+    };
+    getUserData();
+  }, []);
 
   return (
     <>
@@ -16,9 +28,10 @@ const HistoryTransactions = () => {
             <div className={styles.__details}>
               <div className={styles.__left}>
                 <ul>
-                  {userData.transition.map((item, index) => (
+                  {/* {userData.transition.map((item, index) => (
                     <li key={index}>{`${item.name}: ${item.budget}$`}</li>
-                  ))}
+                  ))} */}
+                  <li></li>
                 </ul>
               </div>
             </div>
