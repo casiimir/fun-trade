53096752829e7ac955fd4889ac186fce453c9f96
andy <andy444v@gmail.com>
2023-07-31 17:08:06 +0200

    style/id/page

--- src/pages/cryptovalutes/[id]/index.jsx
+++ src/pages/cryptovalutes/[id]/index.jsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import arrow from "@/assets/iconsProject/arrow.svg";
 import heart from "@/assets/iconsProject/heart.svg";
 import Chart from "@/components/chart";
+import CryptoDatas from "@/mock/cryptoCardMock";
+import Card from "@/components/card";
 
 export default function InfoCard({ data }) {
   const router = useRouter();
@@ -16,42 +18,59 @@ export default function InfoCard({ data }) {
   return (
     <div className={styles.InfoCard}>
       <div className={styles.InfoCard__topBar}>
-        <Image
-          className={styles.InfoCard__arrow}
-          src={arrow}
-          alt="back"
-          width={30}
-          height={30}
-          onClick={onHandleBack}
-        />
+        <div className={styles.InfoCard__arrowContain}>
+          <Image
+            className={styles.InfoCard__arrow}
+            src={arrow}
+            alt="back"
+            width={30}
+            height={30}
+            onClick={onHandleBack}
+          />
+        </div>
         <div className={styles.InfoCard__profile}>
           <div className={styles.InfoCard__container}>
             <div className={styles.InfoCard__imgContain}>
-              <img
-                className={styles.InfoCard__logo}
-                src={data?.image?.small}
-                alt={data.name}
-              />
-            </div>
-            <div className={styles.InfoCard__preferiti}>
-              <Image src={heart} alt="back" width={20} height={20} />
-              <p>aggiungi ai preferiti</p>
+              <div className={styles.InfoCard__imgBorder}>
+                <img
+                  className={styles.InfoCard__logo}
+                  src={data?.image?.small}
+                  alt={data.name}
+                />
+              </div>
             </div>
           </div>
         </div>
+        <div className={styles.InfoCard__preferiti}>
+          <p className={styles.InfoCard__preferitiTitle}>add</p>
+          <Image src={heart} alt="back" width={20} height={20} />
+        </div>
       </div>
-      <h1 className={styles.InfoCard__name}>{data.name}</h1>
-      <div className={styles.InfoCard__infoPrice}>
-        <p>{data?.current_price}</p>
+
+      <div className={styles.InfoCard__containCard}>
+        <div className={styles.InfoCard__charts}>
+          <div className={styles.InfoCard__infoPrice}>
+            <h1 className={styles.InfoCard__name}>{data.name}</h1>
+            <p>{data.last}</p>
+          </div>
+          <Chart />
+        </div>
+        <div className={styles.InfoCard__info}>
+          <h3 className={styles.InfoCard__pName}>Carta d'identità</h3>
+          <p className={styles.InfoCard__description}>
+            {data.description.en.slice(0, 300) + "..."}
+          </p>
+        </div>
       </div>
-      <div className={styles.InfoCard__charts}>
-        <Chart />
-      </div>
-      <div className={styles.InfoCard__info}>
-        <h3 className={styles.InfoCard__pName}>Carta d'identità</h3>
-        <p className={styles.InfoCard__description}>
-          {data.description.en.slice(0, 300) + "..."}
-        </p>
+      <div className={styles.InfoCard__cardContain}>
+        <div>
+          <h2 className={styles.InfoCard__otherCrypto}>Vedi anche</h2>
+          <div className={styles.InfoCard__cryptoList}>
+            {CryptoDatas.map((data) => (
+              <Card data={data} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
