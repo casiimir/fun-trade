5d3fe47881a06ac2aa2c1ee2ab7969f49fc1469f
PeppeTll <peppetll@gmail.com>
2023-07-31 12:01:20 +0200

    feat: localstorage user login

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,15 +3,12 @@ import "@/styles/globals.scss";
 export const UserContext = React.createContext();
 
 export default function App({ Component, pageProps }) {
-  const [userData, setUserData] = useState({});
   const [selectedPage, setSelectedPage] = useState("homepage");
   const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
     <UserContext.Provider
       value={{
-        userData,
-        setUserData,
         selectedPage,
         setSelectedPage,
         isSearchOpen,
